21729fcddb9491cce80213d89171da8a74cfd8b5
Kareem Khazem <karkhaz@amazon.com>
2021-03-31 21:05:20 +0100

    Add support for user-specified outcome table
    This commit adds the --outcome-table flag to the `litani add-job`
    command. This flag allows users to pass in an "outcome table" as a JSON
    file, which litani uses to decide whether a job succeeded or failed.

--- lib/capabilities.py
+++ lib/capabilities.py
@@ -19,6 +19,7 @@ _CAPABILITIES = {
     "report_expire": "Old report directories will contain a .litani-expired file",
     "dir_lock_api": "Deprecated",
     "dir_lock_api_v2": "lib.litani contains the LockableDirectory API",
+    "outcome_table": "The --outcome-table flag is supported",
 }
 
 
