a95c6f5e9ccfde0f4086e7de1dba5d44a0e88ed6
Kareem Khazem <karkhaz@amazon.com>
2021-06-02 18:22:37 +0100

    Move ninja invocation into its own class
    This is in preparation for code that will asynchronously monitor ninja's
    output while it is running.

---
+++ lib/ninja.py
@@ -0,0 +1,58 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+
+import dataclasses
+import pathlib
+import subprocess
+
+
+
+@dataclasses.dataclass
+class Runner:
+    ninja_file: pathlib.Path
+    dry_run: bool
+    parallelism: int
+    pipelines: list
+    ci_stage: str
+    proc: subprocess.CompletedProcess = None
+
+
+    def _get_cmd(self):
+        cmd = [
+            "ninja",
+            "-k", "0",
+            "-f", self.ninja_file,
+        ]
+        if self.parallelism:
+            cmd.extend(["-j", self.parallelism])
+        if self.dry_run:
+            cmd.append("-n")
+
+        if self.pipelines:
+            targets = ["__litani_pipeline_name_%s" % p for p in self.pipelines]
+            cmd.extend(targets)
+        elif self.ci_stage:
+            targets = ["__litani_ci_stage_%s" % p for p in self.ci_stage]
+            cmd.extend(targets)
+
+        return [str(c) for c in cmd]
+
+
+    def run(self):
+        cmd = self._get_cmd()
+        self.proc = subprocess.run(cmd)
+
+
+    def was_successful(self):
+        return not self.proc.returncode
