1faeddeb924596736c40c4f0e8797975866e938e
Kareem Khazem <karkhaz@amazon.com>
2021-07-11 03:50:00 +0100

    Add aux field to run for custom user data
    The intention here is that users can add arbitrary data to a run before
    distributing it, without affecting whether the run validates. Since the
    value of the "aux" key is an unconstrained dict, users can add arbitrary
    keys to it.

--- lib/capabilities.py
+++ lib/capabilities.py
@@ -24,6 +24,7 @@ _CAPABILITIES = {
         "--output-prefix flags are supported",
     "pools": "Jobs can be added to task pools to limit parallelism",
     "memory_profile": "Litani can measure the memory usage of specific jobs",
+    "aux": "Run contains an aux field for custom user data",
 }
 
 
